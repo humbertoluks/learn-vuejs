ee33931126e7ab2aadcd5be658c57d5b5dda123d
humbertoluks <luks.dev@outlook.com>
2021-10-02 11:26:46 -0300

    initial commit

---
+++ 7-vue-component-comunicacao/src/components/FilmesListaIten.vue
@@ -0,0 +1,13 @@
+<template>
+    <li class="list-group-item">
+        <span>{{ filmeTitulo }}</span>
+        <button class="btn btn-success float-right">Editar</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props:['filmeTitulo']
+}
+</script>
+
